08e8aacbc6ec212665fc508df8c1255bded76842
AntumDeluge <antumdeluge@gmail.com>
2017-08-05 15:10:17 -0700

    Add support for 'modconf' mod

--- init.lua
+++ init.lua
@@ -19,23 +19,31 @@
 -- @tfield string modpath Path to the installed mod's location.
 -- @tfield string version Version of this mod.
 value = {}
-value.modname = core.get_current_modname()
-value.modpath = core.get_modpath(value.modname)
+
+
+if core.global_exists('modconf') then
+	modconf.readConfig(value)
+end
+
+if value.name == nil then
+	value.name = core.get_current_modname()
+end
+
+value.path = core.get_modpath(value.name)
 
 
 local scripts = {
-	'config',
 	'settings',
 	'api',
 	'money',
 }
 
 for i, s in ipairs(scripts) do
-	dofile(value.modpath .. '/' .. s .. '.lua')
+	dofile(value.path .. '/' .. s .. '.lua')
 end
 
 
 -- DEBUG:
 if core.settings:get_bool('enable_debug_mods') then
-	core.log('action', 'DEBUG: [' .. value.modname .. '] Version: ' .. tostring(value.version))
+	core.log('action', 'DEBUG: [' .. value.name .. '] Version: ' .. tostring(value.version))
 end
